dabe0a7fc02410b7e43458135dd8726d8f45f3d4
Nick <nicknewburg@gmail.com>
2019-03-20 19:38:58 +0000

    Added the compose button hover and clicked functionality, as well as the jquery slidedown animation for the new tweet container

---
+++ public/scripts/composeButton.js
@@ -0,0 +1,47 @@
+$(function() {
+
+  let clicked = false;
+
+$( ".new-tweet" ).hide()
+
+
+
+
+
+
+  $('.composeBtn').click( function(){
+
+    if(!clicked){
+      $(this).css("opacity", "0.6")
+      clicked = true;
+    } else {
+      $(this).css("opacity", "1")
+      clicked = false;
+    }
+    console.log("click worked");
+
+     $( ".new-tweet" ).slideToggle()
+    // Animation complete.
+    $("#newTweetTxt").focus();
+
+  });
+
+  $( ".composeBtn" ).hover(
+
+  function() {
+    if(!clicked){
+      $(this).css("opacity", "0.6")
+    }
+
+  }, function() {
+    if(!clicked){
+    $(this).css("opacity", "1")
+  }
+  }
+
+);
+
+
+});
+
+
