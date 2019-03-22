77b49d64bf03f51f752fb1c69d2870bad0674552
Nick <nicknewburg@gmail.com>
2019-03-22 02:37:08 +0000

    wrote the README

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,17 @@
 
 $(document).ready(function() {
 
+
+//Hides the error message on load of screen
   $(".error-message").hide();
 
+//jqeury event on submission of the new tweet form
  $('#tweetForm').on('submit',function(e){
     $(".error-message").hide();
     e.preventDefault();
+    //conditional to make sure the text area is not empty or over 140 characters
     if(Number($('.counter').html()) >= 0 && Number($('.counter').html()) !== 140){
-
+    //make the textarea input be transfered into the /tweets route with the ajax method
     let data = $('#tweetForm').serialize();
      $.ajax({
           url:'/tweets',
@@ -24,6 +28,7 @@ $(document).ready(function() {
           }
         });
    } else{
+      //if text area does not meet the if conditional prints the error statements
       $( ".error-message").slideToggle();
       if(Number($('.counter').html()) < 0){
         $('.errorContent').text("You went over the character limit");
@@ -32,7 +37,8 @@ $(document).ready(function() {
       }
    }
   });
-
+      //intakes an array of tweets and calls the create tweet element function on each tweets
+      //prepending them to the tweets container
       function renderTweets(tweets) {
         let temp = "";
           for(let i = 0; i < tweets.length; i++){
@@ -41,47 +47,38 @@ $(document).ready(function() {
           }
       }
 
+      //function to create a DoM element that is appended to the tweets container
       function createTweetElement(tweet){
-        let $tweet = $('<article>').addClass('tweet');
-        let header = $('<header>');
-        let imgContain = $('<div>').addClass("imgContainer");
 
-        let img = $('<img>').attr({
+        let $tweet = $('<article>').addClass('tweet');
+        let header = $('<header>').append($('<div>').addClass("imgContainer").append($('<img>').attr({
           src: tweet.user.avatars.small,
           height: "50px",
           width: "50px",
-        });
-
-        imgContain.append(img);
-        header.append(imgContain);
-
-        let name = $('<h2>').text(tweet.user.name);
-        let handle = $('<span>').text(tweet.user.handle);
-        header.append(name);
-        header.append(handle);
+        })));
+        header.append($('<h2>').text(tweet.user.name));
+        header.append($('<span>').text(tweet.user.handle));
         $tweet.append(header);
 
-        let content = $('<p>').text(tweet.content.text).addClass("tweetContent");
-        $tweet.append(content);
+        $tweet.append($('<p>').text(tweet.content.text).addClass("tweetContent"));
 
         let foot = $('<footer>');
         let time = new Date(tweet.created_at);
-        let dateStamp = $('<span>').text(time.toLocaleString());
-        let flagIcon = $('<img>').addClass("tweetIcon").attr("src", "/images/flag.jpeg" );
-        let retweetIcon = $('<img>').addClass("tweetIcon").attr("src", "/images/retweet.png" );
-        let likeIcon = $('<img>').addClass("tweetIcon").attr("src", "/images/like.png" );
-        foot.append(dateStamp);
-        foot.append(flagIcon);
-        foot.append(retweetIcon);
-        foot.append(likeIcon);
+        foot.append($('<span>').text(time.toLocaleString()));
+        foot.append($('<img>').addClass("tweetIcon").attr("src", "/images/flag.jpeg" ));
+        foot.append($('<img>').addClass("tweetIcon").attr("src", "/images/retweet.png" ));
+        foot.append($('<img>').addClass("tweetIcon").attr("src", "/images/like.png" ));
         $tweet.append(foot);
 
       return $tweet;
       }
 
 
+
+
       let count = 0;
 
+      //function to load the tweets from the /tweets URL then renders them
       function loadTweets(){
 
         $.ajax('/tweets', { method: 'GET' })
--- public/scripts/composeButton.js
+++ public/scripts/composeButton.js
@@ -1,9 +1,14 @@
 $(function() {
 
+  //set a boolean for the button being clicked
   let clicked = false;
+  //hide the compose tweet window on load
+  $( ".new-tweet" ).hide();
 
-$( ".new-tweet" ).hide();
-
+//jquery event for the compose button being clicked
+//if the button isnt clicked its opacity changes to more transparent
+//if it is clicked the button returns to full opacity
+//toggles the new tweet window and focuses on the text area box
   $('.composeBtn').click( function(){
 
     if(!clicked){
@@ -13,13 +18,14 @@ $( ".new-tweet" ).hide();
       $(this).css("opacity", "1");
       clicked = false;
     }
-    console.log("click worked");
 
      $( ".new-tweet" ).slideToggle()
     $("#newTweetTxt").focus();
 
   });
 
+//jquery hover function for changing the opacity
+//if it is clicked it has no effect
   $( ".composeBtn" ).hover(
     function() {
       if(!clicked){
@@ -33,6 +39,7 @@ $( ".new-tweet" ).hide();
     }
   );
 
+//jquery event for highlighting the login button on hover
    $( ".login" ).hover(
     function() {
       $(this).css("opacity", "0.6");
